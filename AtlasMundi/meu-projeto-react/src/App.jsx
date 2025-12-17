35a1313eb21f4ba9647a92f37c6a25b22659b300
LiviaSobral <liviaheloisa2005@gmail.com>
2025-12-17 15:48:58 -0300

    AJEITADO O erro nas telas

--- AtlasMundi/meu-projeto-react/src/App.jsx
+++ AtlasMundi/meu-projeto-react/src/App.jsx
@@ -210,7 +210,7 @@ const App = () => {
           }
         />
 
-        <Route path="/historia"  element={<Historia pais={nomePais} english={english} flagUrl={flagUrl} />}/>
+        <Route path="/historia"  element={<Historia pais={nomePais} flagUrl={flagUrl} english={english}/>}/>
         <Route path="/politica" element={<Politica pais={nomePais} english={english} />} />
         <Route path="/cultura" element={<Cultura pais={nomePais} english={english} />} />
         <Route path="/comentarios" element={<Comentarios pais={nomePais} english={english} />} />
