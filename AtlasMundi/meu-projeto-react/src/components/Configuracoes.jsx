6b8f1c29c90c45944882df3e563eeb5a1113eee2
unknown <joaopedro.backxavier@gmail.com>
2026-01-05 11:23:35 -0300

    botao de aumentar e diminuir fonte

--- AtlasMundi/meu-projeto-react/src/components/Configuracoes.jsx
+++ AtlasMundi/meu-projeto-react/src/components/Configuracoes.jsx
@@ -14,14 +14,31 @@ const Configuracoes = ({ onClose, english,toggleLanguage }) => {
     p: 4 
   };
 
+function alterarFonte(fator) {
+  const html = document.documentElement;
+
+  const atual = parseFloat(
+    getComputedStyle(html).fontSize
+  );
+
+  const novo = atual * fator;
+
+  // limites
+  if (novo > 30 || novo < 8.5) return;
+
+  html.style.fontSize = `${novo}px`;
+}
+
+
+
   return (
     <Modal open={true} onClose={onClose}>
       <Box sx={modalStyle}>
         <Typography variant="h6" gutterBottom>{english ? "Configurations" : "Configurações"}</Typography>
         <div className="config-buttons">
           <Button fullWidth sx={{ mt: 1 }} onClick={toggleLanguage} variant="contained">{english ? "Change to Portuguese" : "Mudar para Inglês"}</Button>
-          <Button fullWidth sx={{ mt: 1 }} variant="contained">{english ? "Increase font size" : "Aumentar fonte"}</Button>
-          <Button fullWidth sx={{ mt: 1 }} variant="contained">{english ? "Decrease font size" : "Diminuir fonte"}</Button>
+          <Button onClick={() => {alterarFonte(1.1)}} fullWidth sx={{ mt: 1 }} variant="contained">{english ? "Increase font size" : "Aumentar fonte"}</Button>
+          <Button onClick={() => {alterarFonte(0.9)}} fullWidth sx={{ mt: 1 }} variant="contained">{english ? "Decrease font size" : "Diminuir fonte"}</Button>
           <Button fullWidth sx={{ mt: 2 }} onClick={onClose}>{english ? "Close" : "Fechar"}</Button>
         </div>
       </Box>
